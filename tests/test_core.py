fa6e15bcfe83272501912bfe09fa360c64422559
popopoter <troteaga3@gmail.com>
2017-03-16 21:38:08 +0100

    Refactor

--- tests/test_core.py
+++ tests/test_core.py
@@ -8,79 +8,86 @@ import unittest
 class CoreTestSuite(unittest.TestCase):
     """Test cases for Core"""
     def test_countTwoWords(self):
-        result = sample.analIce("patta macarron")
-        self.assertEqual(len(result),2)
+        self.assertEqual(sample.analIce("patta macarron"),[('macarron',1),('patta',1)])
 
     def test_groupSameWord(self):
         result = sample.analIce("patta patta")
-        self.assertEqual(len(result),1)
-
-    def test_countSameWordTwoTimes(self):
-        result = sample.analIce("patta patta")
-        self.assertEqual(result[0][1],2)
+        self.assertEqual(sample.analIce("patta patta"),[('patta',2)])
 
     def test_emptyInputString(self):
-        self.assertEqual(len(sample.analIce("")),0)
+        self.assertEqual((sample.analIce("")),[])
 
     def test_longInputString(self):
         numberOfMayors = 10000
         longString = ""
         for i in range(0,numberOfMayors):
             longString = longString + "Es el alcalde el que quiere que sean los vecinos el alcalde "
-        self.assertEqual(sample.analIce(longString)[1][0],'alcalde')
-        self.assertEqual(sample.analIce(longString)[1][1],numberOfMayors*2)
 
-    def test_resultSortingCount(self):
-        result = sample.analIce("tres tres tres dos dos uno")
-        self.assertEqual(result[0][1],3)
-        self.assertEqual(result[1][1],2)
-        self.assertEqual(result[2][1],1)
+        self.assertEqual(sample.analIce(longString),[(u'el', (numberOfMayors*3)),
+                                                         (u'alcalde', numberOfMayors*2), (u'que', numberOfMayors*2), (u'quiere', numberOfMayors),
+                                                         (u'los', numberOfMayors), (u'vecinos', numberOfMayors),(u'sean', numberOfMayors), (u'es', numberOfMayors)])
 
     def test_resultSortingValues(self):
-        result = sample.analIce("tres tres tres dos dos uno")
-        self.assertEqual(result[0][0],'tres')
-        self.assertEqual(result[1][0],'dos')
-        self.assertEqual(result[2][0],'uno')
+        self.assertEqual(sample.analIce("tres tres tres dos dos uno"),[('tres',3),('dos',2),('uno',1)])
 
+    def test_NoneInput(self):
+        with self.assertRaises(TypeError):
+            sample.analIce(None)
+
+    #Test de simbolos
     def test_question(self):
         result= sample.analIce("¿dónde esta la lista?")
         self.assertEqual(len(result),4)
+
     def test_simbolNotIncluded(self):
         result = sample.analIce(".:...:.:::.:?¿!perro")
         self.assertEqual(result[0][0],'perro')
         self.assertEqual(len(result),1)
+
     def test_simbolSeparateWords(self):
         result = sample.analIce("verificar!pudiendo?verificar")
         self.assertEqual(result[0][0],'verificar')
         self.assertEqual(result[0][1],2)
         self.assertEqual(result[1][0], 'pudiendo')
         self.assertEqual(len(result),2)
+
     def test_onlySimbols(self):
         result = sample.analIce("::::,!¡¿?[]{}();")
         self.assertEqual(len(result),0)
+
     def test_apostrophe(self):
         result = sample.analIce("y'all wanna kill y'all?")
         self.assertEqual(len(result), 3)
         self.assertEqual(result[0][0],"y'all")
+
+
     def test_accentMark(self):
         result = sample.analIce("Quién")
         self.assertEqual(result[0][0], u"quién")
+
     def test_accentCaps(self):
         result = sample.analIce("QuiÉn")
         self.assertEqual(result[0][0], u"quién")
+
     def test_email(self):
         result = sample.analIce("juan.gomez@live.u-tad.com")
         self.assertEqual(len(result),6)
+
     def test_tabulation(self):
         result = sample.analIce("hola   que tal ")
         self.assertEqual(len(result), 3)
+
     def test_dieresis(self):
         result = sample.analIce("pingüino")
         self.assertEqual(result[0][0],u"pingüino")
+
     def test_virgulilla(self):
         result = sample.analIce("años")
         self.assertEqual(result[0][0],u"años")
+    def test_backslash(self):
+        self.assertEqual(sample.analIce(r'\ja\monorail')[1][0],'monorail')
 
+    #Test de StopWords
     def test_countStopword(self):
         result = sample.analIce("about all")
         self.assertEqual(len(result), 0)
