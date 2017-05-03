98199917b05c61619cd875aa0c917b39022359fb
popopoter <troteaga3@gmail.com>
2017-05-03 23:14:06 +0200

    test
    2 test selenium web driver

--- Analice/tests.py
+++ Analice/tests.py
@@ -2,5 +2,23 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
-
 # Create your tests here.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+driver = webdriver.Firefox(executable_path=r'C:\Users\Me\sel\geckodriver.exe')
+driver.get('http://127.0.0.1:8000/')
+head = driver.find_element_by_id('id_text_content')
+head.send_keys('Locomia' + Keys.RETURN)
+time.sleep(2)
+head = driver.find_element_by_id('btn_reset').click()
+
+
+driver = webdriver.Firefox(executable_path=r'C:\Users\Me\sel\geckodriver.exe')
+driver.get('http://127.0.0.1:8000/')
+head = driver.find_element_by_id('id_text_content')
+head.send_keys('Locomia Locomia Locomia' + Keys.RETURN)
+time.sleep(2)
+head = driver.find_element_by_id('btn_execute').click()
+
+#masthead-search-term
